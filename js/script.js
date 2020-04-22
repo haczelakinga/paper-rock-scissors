611c1b1356f755d2d411ec61a12d33159df99bc4
Kinga Haczela <haczelakinga@gmail.com>
2020-04-22 14:13:30 +0200

    Refactor player choice function

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 const rock = document.querySelector("img.rock");
 const paper = document.querySelector("img.paper");
 const scissors = document.querySelector("img.scissors");
-const computerImg = document.querySelector("img.computer-img");
-const playerImg = document.querySelector("img.player-img");
+let computerImg = document.querySelector("img.computer-img");
+let playerImg = document.querySelector("img.player-img");
 const result = document.querySelector("p.result");
 const resultNumbers = document.querySelector("p.result-numbers");
 const clearBtn = document.querySelector("button.clear-btn");
@@ -13,45 +13,50 @@ let computerWonGames = 0;
 
 //Player choice
 rock.addEventListener("click", function() {
-    playerChoice = 'rock';
-    playerImg.src='../images/rock.png';
-    computer();
-    checkResult();
-    clearBtn.style.display= "block";  
+    buttonClicked("rock")
 });
 
 paper.addEventListener("click", function() {
-    playerChoice = 'paper';
-    playerImg.src='../images/paper.png';
-    clearBtn.style.display = "block";
-    computer();
-    checkResult();
-    clearBtn.style.display= "block";  
+    buttonClicked("paper")
 });
 
 scissors.addEventListener("click", function() {
-    playerChoice = 'scissors';
-    playerImg.src='../images/scissors.png';
-    clearBtn.style.display = "block";
-    computer();
-    checkResult();
-    clearBtn.style.display= "block";  
+    buttonClicked("scissors")
 });
 
+//Clear game
+clearBtn.addEventListener("click", function() {
+    playerWonGames = 0;
+    computerWonGames = 0;
+    resultNumbers.innerHTML = `${playerWonGames} : ${computerWonGames}`;
+    result.innerHTML = "Let's start the game!<br>Choose your move!";
+    playerImg.src = '../images/question-mark.png';
+    computerImg.src = '../images/question-mark.png';
+    clearBtn.style.display = "none";
+});
+
+function buttonClicked(btnName) {
+    console.log('clicked');
+    playerChoice = btnName;
+    playerImg.src = `../images/${btnName}.png`;
+    clearBtn.style.display= "block";  
+    computerMove();
+    checkResult();
+}
 
 //Computer choice
-function computer() {
+function computerMove() {
     let randomNumber = Math.floor(Math.random() * 3 + 1);
- if (randomNumber == 1) {
-     computerImg.src='../images/paper.png';
-     computerChoice = 'paper';
- } else if (randomNumber == 2) {
-    computerImg.src='../images/rock.png';
-    computerChoice = 'rock'
- } else {
-    computerImg.src='../images/scissors.png';
-    computerChoice = 'scissors';
- }
+    if (randomNumber == 1) {
+        computerImg.src='../images/paper.png';
+        computerChoice = 'paper';
+    } else if (randomNumber == 2) {
+        computerImg.src='../images/rock.png';
+        computerChoice = 'rock'
+    } else {
+        computerImg.src='../images/scissors.png';
+        computerChoice = 'scissors';
+    }
 }
 
 //Result check
@@ -76,15 +81,4 @@ function checkResult() {
         resultNumbers.innerHTML = `${playerWonGames} : ${computerWonGames}`;
     }
 }
-
-//Clear game
-clearBtn.addEventListener("click", function() {
-    playerWonGames = 0;
-    computerWonGames = 0;
-    resultNumbers.innerHTML = `${playerWonGames} : ${computerWonGames}`;
-    result.innerHTML = "Let's start the game!<br>Choose your move!";
-    playerImg.src = '../images/question-mark.png';
-    computerImg.src = '../images/question-mark.png';
-    clearBtn.style.display = "none";
-});
 
