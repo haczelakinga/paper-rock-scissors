e3ef6beff4a8291ecccc178df4463e01155f7a02
Kinga Haczela <haczelakinga@gmail.com>
2020-04-24 08:34:18 +0200

    Paths for images adjusted

--- js/script.js
+++ js/script.js
@@ -30,15 +30,15 @@ clearBtn.addEventListener("click", function() {
     computerWonGames = 0;
     resultNumbers.innerHTML = `${playerWonGames} : ${computerWonGames}`;
     result.innerHTML = "Let's start the game!<br>Choose your move!";
-    playerImg.src = '../images/question-mark.png';
-    computerImg.src = '../images/question-mark.png';
+    playerImg.src = 'images/question-mark.png';
+    computerImg.src = 'images/question-mark.png';
     clearBtn.style.display = "none";
 });
 
 function buttonClicked(btnName) {
     console.log('clicked');
     playerChoice = btnName;
-    playerImg.src = `../images/${btnName}.png`;
+    playerImg.src = `images/${btnName}.png`;
     clearBtn.style.display= "block";  
     computerMove();
     checkResult();
@@ -48,13 +48,13 @@ function buttonClicked(btnName) {
 function computerMove() {
     let randomNumber = Math.floor(Math.random() * 3 + 1);
     if (randomNumber == 1) {
-        computerImg.src='../images/paper.png';
+        computerImg.src='images/paper.png';
         computerChoice = 'paper';
     } else if (randomNumber == 2) {
-        computerImg.src='../images/rock.png';
+        computerImg.src='images/rock.png';
         computerChoice = 'rock'
     } else {
-        computerImg.src='../images/scissors.png';
+        computerImg.src='images/scissors.png';
         computerChoice = 'scissors';
     }
 }
